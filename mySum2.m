a7ec6afa1622e8b6f039d01e2670ee2e9ffa161f
VAMMR <42310216+vammr@users.noreply.github.com>
2021-04-27 22:08:48 +0400

    Sum & Sphere Radius Functions

---
+++ mySum2.m
@@ -0,0 +1,14 @@
+function [ result ] = mySum2( in1, in2)
+result = in1 + in2;
+end 
+
+function [result ] = myProd2( in1, in2, in3)
+result = in1 * in2 * in3;
+end 
+
+function [ s,d,p,r] = myOperation2 (in1, in2)
+s = in1+in2
+d = in1-in2
+p = in1*in2
+r=in1/in2
+end 
---
+++ sphereRadius.m
@@ -0,0 +1,4 @@
+function volume = sphereRadius (radius)
+volume = 4/3*pi*(radius)^3);
+end 
+%radius = 1.2345
